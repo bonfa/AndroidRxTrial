484de98aa898aae38a94784b33fb32e02e33facc
Francesco Bonfadelli <francesco.bonfadelli@lastminute.com>
2020-03-30 12:28:19 +0400

    create WeatherServiceFactory

--- Todo.md
+++ Todo.md
@@ -2,7 +2,7 @@
 [x] Reload on button clickable
 [x] Add loaders
 [] Network Cache
+[] try another flow making the network updating the db and then the ui reacting to the db
 [] Add search + list + detail
 [] UI
 [] Adapting
-[] try another flow making the network updating the db and then the ui reacting to the db
---
+++ app/src/main/java/fbonfadelli/it/networktrial/WeatherServiceFactory.kt
@@ -0,0 +1,41 @@
+package fbonfadelli.it.networktrial
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+private const val BASE_URL = "https://www.metaweather.com/"
+
+class WeatherServiceFactory {
+    companion object {
+        fun make(): WeatherApi {
+            val retrofit = Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .client(createHttpClient())
+                .build()
+
+            return retrofit.create(WeatherApi::class.java)
+        }
+
+        private fun createHttpClient(): OkHttpClient {
+            return OkHttpClient.Builder()
+                .readTimeout(15L, TimeUnit.SECONDS)
+                .writeTimeout(15L, TimeUnit.SECONDS)
+                .connectTimeout(15L, TimeUnit.SECONDS)
+                .addInterceptor(loggerInterceptor())
+                .build()
+        }
+
+        private fun loggerInterceptor(): HttpLoggingInterceptor {
+            val interceptor = HttpLoggingInterceptor()
+            interceptor.level =
+                if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE
+            return interceptor
+        }
+    }
+}
